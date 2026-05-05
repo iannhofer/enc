ef4d22a0f9e36d03d84ad8eb07b98498e02cc2b2
IanHofer <ianaotohofer@gmail.com>
2026-05-05 23:42:57 +0200

    passwords are hashed. fixed small errors.

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,8 +1,12 @@
 import sqlite3
 from pathlib import Path
+from passlib.context import CryptContext
+
+pwd_context=CryptContext(schemes=["bcrypt"], deprecated= "auto")
+
 
 def createDb():
-    with sqlite3.connect("users.db") as conn:
+    with sqlite3.connect("storage.db") as conn:
         cursor = conn.cursor()
         cursor.execute(
             """
@@ -10,57 +14,59 @@ def createDb():
                 id integer primary key autoincrement,
                 username text unique not null,
                 password text not null
-            )
-            """
-        )
-        conn.commit()
-        print("Database and table created")
-    with sqlite3.connect("files.db") as conn:
-        cursor = conn.cursor()
+            )""")
         cursor.execute(
             """
             create table if not exists files(
-                id integer primary key autoincrement,
-                userId integer not null,
-                filename text not null
-            )
-            """
-        )
+            id integer primary key autoincrement,
+            userId integer not null,
+            filename text not null
+        )""")
+        conn.commit()
+        print("Database and table created")
+
+
+
 def authenticateUser(username, password):
     dbCheck()
-    with sqlite3.connect("users.db") as conn:
+    with sqlite3.connect("storage.db") as conn:
         cursor = conn.cursor()
         cursor.execute(
             """
-            select * from users where username = ? and password = ?
+            select * from users where username = ?
             """,
-            (username, password)
+            (username,)
         )
         user = cursor.fetchone()
-        return user
+        if user:
+            hashedPw = user[2]
+            if pwd_context.verify(password, hashedPw):
+                return user
+        return None
 
 def createUser(username, password):
     dbCheck()
-    with sqlite3.connect("users.db") as conn:
+    hashedPw = pwd_context.hash(password)
+    with sqlite3.connect("storage.db") as conn:
         cursor = conn.cursor()
         cursor.execute("""
         select * from users where username = ?""", (username,))
         if not cursor.fetchone():
             cursor.execute("""
             insert into users (username, password) values (?, ?)
-            """, (username, password))
+            """, (username, hashedPw))
             conn.commit()
             return authenticateUser(username, password)
         return None
 
 def getUserFiles(user_id):
     dbCheck()
-    with sqlite3.connect("users.db") as conn:
+    with sqlite3.connect("storage.db") as conn:
         cursor = conn.cursor()
-        cursor.execute("select filename from files where user_id = ?", (user_id,))
+        cursor.execute("select filename from files where userId = ?", (user_id,))
         files = [row[0] for row in cursor.fetchall()]
         return files
 
 def dbCheck():
-    if not Path("users.db").is_file():
+    if not Path("storage.db").is_file():
         createDb()
